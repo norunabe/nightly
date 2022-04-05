c77449001fda46e022d9a24693fae210938114f6
anmitutarow <anmitutarow@gmail.com>
2022-04-05 11:57:33 +0900

    add javascript audio control

--- Ghost/js/main.js
+++ Ghost/js/main.js
@@ -1,4 +1,15 @@
-let ghost = new Audio("../audio/ghost.mp3");
-setTimeout(() => {
-    ghost.play();
-}, 1000);
+let judge = false;
+document.getElementById("trap").addEventListener("click",() => {
+    switch(judge){
+        case false:
+            let ghost = new Audio("../audio/ghost.mp3");
+            setTimeout(() => {
+                ghost.play();
+            }, 1000);
+            judge = true;
+            break;
+        default:
+            location.href="https://www.google.com";
+            break;
+    }
+});
