5c22206f793391a574cfbbfd229171c546fb8d79
anmitutarow <anmitutarow@gmail.com>
2022-04-06 20:31:40 +0900

    Hello function

--- pbt/react/game.jsx
+++ pbt/react/game.jsx
@@ -1,8 +1,7 @@
 //初期設定の表示
-class First extends React.Component{
+class Welcome extends React.Component{
     render(){
         return(
-            //返すHTML
             <div className="fade-in">
                 <p className="fs-3 gradation-color">みなさんはじめまして</p>
                 <p>プレイヤーの名前を決めてください</p>
@@ -15,7 +14,7 @@ class First extends React.Component{
     }
 }
 //通常処理
-class Every extends React.Component{
+class Hello extends React.Component{
     render(){
         return(
             <p className="fs-3 fade-in-out">Hello!?! {localStorage.getItem("id")}</p>
@@ -23,26 +22,29 @@ class Every extends React.Component{
     }
 }
 
+function React_Every() {
+    ReactDOM.render(
+        <Hello />,
+        document.getElementById("text")
+    );
+}
+
+
 let id = localStorage.getItem("id");
 switch(id){
     //IDがない時は登録画面を表示する
     case null:
         ReactDOM.render(
-            <First />,
+            <Welcome />,
             document.getElementById("text")
         );
         document.getElementById("ok").addEventListener("click",() => {
             localStorage.setItem("id",document.getElementById("q").value);
-            ReactDOM.render(
-                <Every />,
-                document.getElementById("text")
-            );
+            id = localStorage.getItem("id");
+            React_Every();
         });
         break;
     default:
-        ReactDOM.render(
-            <Every />,
-            document.getElementById("text")
-        );
+        React_Every();
         break;
 }
--- pbt/src/game.js
+++ pbt/src/game.js
@@ -7,59 +7,56 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 //初期設定の表示
-var First = function (_React$Component) {
-    _inherits(First, _React$Component);
+var Welcome = function (_React$Component) {
+    _inherits(Welcome, _React$Component);
 
-    function First() {
-        _classCallCheck(this, First);
+    function Welcome() {
+        _classCallCheck(this, Welcome);
 
-        return _possibleConstructorReturn(this, (First.__proto__ || Object.getPrototypeOf(First)).apply(this, arguments));
+        return _possibleConstructorReturn(this, (Welcome.__proto__ || Object.getPrototypeOf(Welcome)).apply(this, arguments));
     }
 
-    _createClass(First, [{
+    _createClass(Welcome, [{
         key: "render",
         value: function render() {
-            return (
-                //返すHTML
+            return React.createElement(
+                "div",
+                { className: "fade-in" },
+                React.createElement(
+                    "p",
+                    { className: "fs-3 gradation-color" },
+                    "\u307F\u306A\u3055\u3093\u306F\u3058\u3081\u307E\u3057\u3066"
+                ),
+                React.createElement(
+                    "p",
+                    null,
+                    "\u30D7\u30EC\u30A4\u30E4\u30FC\u306E\u540D\u524D\u3092\u6C7A\u3081\u3066\u304F\u3060\u3055\u3044"
+                ),
                 React.createElement(
                     "div",
-                    { className: "fade-in" },
-                    React.createElement(
-                        "p",
-                        { className: "fs-3 gradation-color" },
-                        "\u307F\u306A\u3055\u3093\u306F\u3058\u3081\u307E\u3057\u3066"
-                    ),
-                    React.createElement(
-                        "p",
-                        null,
-                        "\u30D7\u30EC\u30A4\u30E4\u30FC\u306E\u540D\u524D\u3092\u6C7A\u3081\u3066\u304F\u3060\u3055\u3044"
-                    ),
-                    React.createElement(
-                        "div",
-                        { className: "input-group mb-3" },
-                        React.createElement("input", { type: "text", className: "form-control", placeholder: "\u597D\u304D\u306A\u540D\u524D\u3092\u5165\u529B", "aria-label": "\u597D\u304D\u306A\u540D\u524D\u3092\u5165\u529B", id: "q" }),
-                        React.createElement("input", { type: "button", value: "OK!", className: "btn btn-outline-light", id: "ok" })
-                    )
+                    { className: "input-group mb-3" },
+                    React.createElement("input", { type: "text", className: "form-control", placeholder: "\u597D\u304D\u306A\u540D\u524D\u3092\u5165\u529B", "aria-label": "\u597D\u304D\u306A\u540D\u524D\u3092\u5165\u529B", id: "q" }),
+                    React.createElement("input", { type: "button", value: "OK!", className: "btn btn-outline-light", id: "ok" })
                 )
             );
         }
     }]);
 
-    return First;
+    return Welcome;
 }(React.Component);
 //通常処理
 
 
-var Every = function (_React$Component2) {
-    _inherits(Every, _React$Component2);
+var Hello = function (_React$Component2) {
+    _inherits(Hello, _React$Component2);
 
-    function Every() {
-        _classCallCheck(this, Every);
+    function Hello() {
+        _classCallCheck(this, Hello);
 
-        return _possibleConstructorReturn(this, (Every.__proto__ || Object.getPrototypeOf(Every)).apply(this, arguments));
+        return _possibleConstructorReturn(this, (Hello.__proto__ || Object.getPrototypeOf(Hello)).apply(this, arguments));
     }
 
-    _createClass(Every, [{
+    _createClass(Hello, [{
         key: "render",
         value: function render() {
             return React.createElement(
@@ -71,20 +68,25 @@ var Every = function (_React$Component2) {
         }
     }]);
 
-    return Every;
+    return Hello;
 }(React.Component);
+
+function React_Every() {
+    ReactDOM.render(React.createElement(Hello, null), document.getElementById("text"));
+}
 
 var id = localStorage.getItem("id");
 switch (id) {
     //IDがない時は登録画面を表示する
     case null:
-        ReactDOM.render(React.createElement(First, null), document.getElementById("text"));
+        ReactDOM.render(React.createElement(Welcome, null), document.getElementById("text"));
         document.getElementById("ok").addEventListener("click", function () {
             localStorage.setItem("id", document.getElementById("q").value);
-            ReactDOM.render(React.createElement(Every, null), document.getElementById("text"));
+            id = localStorage.getItem("id");
+            React_Every();
         });
         break;
     default:
-        ReactDOM.render(React.createElement(Every, null), document.getElementById("text"));
+        React_Every();
         break;
 }
