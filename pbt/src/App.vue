a9870e8154df918b7257253f342eb169fd132094
anmitutarow <anmitutarow@gmail.com>
2022-04-09 19:30:53 +0900

    add framework file

---
+++ pbt/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <MainIndex />
+</template>
+
+<script>
+import MainIndex from './components/MainIndex.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainIndex
+  }
+}
+</script>
