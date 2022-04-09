7eff3cf819db6a7570b6bd49dc60143bc7252188
anmitutarow <anmitutarow@gmail.com>
2022-04-09 20:08:54 +0900

    animation css

--- pbt/src/App.vue
+++ pbt/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="container-fluid bg-secondary text-center text-light">
-      <div class="row justify-content-center">
-          <div class="col-12 col-sm-10 col-md-8 col-lg-6 bg-dark text-break" id="root">
-              <div id="game" class="d-flex align-items-center justify-content-center"></div>
-              <div id="text" class="d-flex align-items-center justify-content-center"></div>
-          </div>
-      </div>
-  </div>
+    <div class="container-fluid bg-secondary text-center text-light">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-10 col-md-8 col-lg-6 bg-dark text-break" id="root">
+                <div id="game" class="d-flex align-items-center justify-content-center"></div>
+                <div id="text" class="d-flex align-items-center justify-content-center"></div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+    name: 'App'
 }
 </script>
 
--- pbt/src/components/WelcomeVue.vue
+++ pbt/src/components/WelcomeVue.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
+    <div class="fade-in">
         <p class="fs-3">はじめまして</p>
+        <p>プレイヤーの名前を入力してください</p>
         <div class="input-group mb-3">
             <input type="text" class="form-control" placeholder="名前を入力">
             <input type="button" class="btn btn-outline-secondary" value="OK!">
