0eb5816ea4f6f3962fa1cfd5c4bc39a0dc64e691
anmitutarow <anmitutarow@gmail.com>
2022-04-09 19:57:18 +0900

    login vue

---
+++ pbt/src/components/WelcomeVue.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p class="fs-3">はじめまして</p>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="名前を入力">
+            <input type="button" class="btn btn-outline-secondary" value="OK!">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"WelcomeVue"
+}
+</script>
--- pbt/src/main.js
+++ pbt/src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
+import Welcome from "./components/WelcomeVue.vue";
 
-createApp(App).mount('#app')
+//メインフレームワーク構成
+createApp(App).mount("#app");
+
+
+//ログイン管理
+let id;
+id = localStorage.getItem("id");
+switch (id) {
+    case null:
+        createApp(Welcome).mount("#text");
+}
