18685730b5a6a5fb3bda600fd9c2f3d285cf8ba9
anmitutarow <anmitutarow@gmail.com>
2022-04-09 20:35:34 +0900

    add localstorage set

--- pbt/src/components/WelcomeVue.vue
+++ pbt/src/components/WelcomeVue.vue
@@ -3,14 +3,19 @@
         <p class="fs-3">はじめまして</p>
         <p>プレイヤーの名前を入力してください</p>
         <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="名前を入力">
-            <input type="button" class="btn btn-outline-secondary" value="OK!">
+            <input type="text" class="form-control" placeholder="名前を入力" id="q">
+            <input type="button" class="btn btn-outline-secondary" value="OK!" @click="set_id()">
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name:"WelcomeVue"
+    name : "WelcomeVue",
+    methods : {
+        set_id : () => {
+            localStorage.setItem("id",document.getElementById("q").value)
+        }
+    }
 }
 </script>
