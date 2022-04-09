faac6dd8d87440c45f1ecd369f72714d77892d43
anmitutarow <anmitutarow@gmail.com>
2022-04-09 16:24:02 +0900

    use vue cli

---
+++ pbt-old/js/game.js
@@ -0,0 +1,5 @@
+import Welcome from "vue/Welcome.vue";
+
+export default {
+    components : {Welcome}
+};
---
+++ pbt-old/js/vue/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Test</h1>
+</template>
+<script>
+export default {
+    data(){
+        return;
+    }
+}
+</script>
+<style scoped>
+
+</style>
